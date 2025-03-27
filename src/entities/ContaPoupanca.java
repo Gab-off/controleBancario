8fd698611545f3e1ac44fefad9a792a959bfbe65
Gab-off <albuquerque1.gabriel@gmail.com>
2025-03-27 12:07:49 -0300

    Finalizado a conta poupança

--- src/entities/ContaPoupanca.java
+++ src/entities/ContaPoupanca.java
@@ -1,4 +1,37 @@
 package entities;
 
-public class ContaPoupanca {
+import exceptions.ValorMenorException;
+
+public class ContaPoupanca extends Conta {
+    private double reajusteMensal;
+
+    public ContaPoupanca() {
+    }
+
+    public ContaPoupanca(double reajusteMensal) {
+        this.reajusteMensal = reajusteMensal;
+    }
+
+    public ContaPoupanca(double saldo, double reajusteMensal) {
+        super(saldo);
+        this.reajusteMensal = reajusteMensal;
+    }
+
+    public double getReajusteMensal() {
+        return reajusteMensal;
+    }
+
+    public void setReajusteMensal(double reajusteMensal) {
+        this.reajusteMensal = reajusteMensal;
+    }
+
+    public void atualizarSaldo(double reajusteMensal) {
+        if (reajusteMensal <= 0) {
+            throw new ValorMenorException("Porcentagem do reajuste deve ser maior que 0 (ZERO).");
+        }
+        
+        this.reajusteMensal = reajusteMensal;
+        double rendimento = this.saldo * reajusteMensal;
+        this.saldo += rendimento;
+    }
 }
